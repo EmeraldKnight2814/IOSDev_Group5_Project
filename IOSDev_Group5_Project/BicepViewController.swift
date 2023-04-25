0588149be71c70e405dc58ce85e0d4b2e5b93fb3
Alex Hernandez <achern19@yahoo.com>
2023-04-25 12:47:03 -0500

    BicepView
    -Added connections and UI elements for Bicep View

--- IOSDev_Group5_Project/BicepViewController.swift
+++ IOSDev_Group5_Project/BicepViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class BicepViewController: UIViewController {
+    //Outlet for top label so I can change it with whichever workout is clicked
+        @IBOutlet weak var WorkoutTitleOL: UILabel!
+        //Button For exercise1
+        @IBOutlet weak var FirstExerciseBTN: UIButton!
+        //button for exercise2
+        @IBOutlet weak var SecondExerciseBTN: UIButton!
+        //button for exercise3
+        @IBOutlet weak var ThirdExerciseBTN: UIButton!
+        //Description Outlet for selected workout
+        @IBOutlet weak var WorkoutDescriptionOL: UILabel!
+        //Workout image Outlet
+        @IBOutlet weak var WorkoutIMGView: UIImageView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        //when First exercise button is pressed
+@IBAction func ExerciseOneBTNPressed(_ sender: Any) {
+            //when button 1 is pressed, hide all other buttons, display correlated image in the view, and display text in workoutdesc OL
+            //repeat for button two and three
+        }
+        //when Second exercise button is pressed
+@IBAction func ExerciseTwoBTNPressed(_ sender: Any) {
+        }
+        
+        //when Third Exercise button is pressed
+@IBAction func ExerciseThreeBTNPressed(_ sender: Any) {
+        }
+        
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
