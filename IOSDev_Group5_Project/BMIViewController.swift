48cb792282e14cc659eedc5c1d6673906b7247da
Daniel J Crowley <61795248+TheMusicMan28@users.noreply.github.com>
2023-04-25 10:27:09 -0500

    added BMI View controller

---
+++ IOSDev_Group5_Project/BMIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BMIViewController.swift
+//  IOSDev_Group5_Project
+//
+//  Created by Student on 4/25/23.
+//
+
+import UIKit
+
+class BMIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
