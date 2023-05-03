884511f4689851e6f3d1901f2634ebc29a786077
Amirsha Kabulov <kabulovamirsha@gmail.com>
2023-05-03 18:35:24 -0500

    Added chest workout

--- IOSDev_Group5_Project/ChestViewController.swift
+++ IOSDev_Group5_Project/ChestViewController.swift
@@ -9,12 +9,40 @@ import UIKit
 
 class ChestViewController: UIViewController {
 
+    @IBAction func exercise1Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func exercise2Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func description1Pressed(_ sender: Any) {
+        imageDescription.image = UIImage(named: "chest1")
+        ExerciseDescription.text = "This is exercise works the whole chest. Perform the dumbbell bench press exercise by lying flat on your back on a bench. Grab a pair of dumbbells and hold them on your hips.Squeeze your shoulder blades as you lower them again until your upper and lower arms form a 90-degree angle."
+    }
+    @IBAction func description2Pressed(_ sender: Any) {
+        imageDescription.image = UIImage(named: "chest2")
+        ExerciseDescription.text = "While traditional pushups work your chest, arms, and shoulders, incline pushups take some of the pressure off your arms and shoulders to give you a solid upper-chest workout. Just find an object about 3-5 inches high, put your legs on top and start the push ups."
+    }
+    
+    @IBOutlet weak var imageDescription: UIImageView!
+    
+    @IBOutlet weak var ExerciseDescription: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showExercise1" {
+            let showViewController = segue.destination as? ShowViewController
+            showViewController?.videoName = "chest1"
+        }
+           if segue.identifier == "showExercise2" {
+               let showViewController = segue.destination as? ShowViewController
+               showViewController?.videoName = "chest2"
+           }
+       }
+   }
 
     /*
     // MARK: - Navigation
@@ -25,5 +53,3 @@ class ChestViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
---
+++ IOSDev_Group5_Project/ShowViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ShowViewController.swift
+//  IOSDev_Group5_Project
+//
+//  Created by Student on 5/3/23.
+//
+import AVKit
+import AVFoundation
+import UIKit
+
+class ShowViewController: UIViewController {
+    var videoName: String?
+    var videoName2: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let videoName = videoName, let videoURL = Bundle.main.url(forResource: videoName, withExtension: "mov") {
+            let player = AVPlayer(url: videoURL)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            playerViewController.view.frame = view.bounds
+            playerViewController.showsPlaybackControls = true
+            
+            addChild(playerViewController)
+            view.addSubview(playerViewController.view)
+            playerViewController.didMove(toParent: self)
+            
+            player.play()
+        }
+        
+        if let videoName2 = videoName2, let videoURL2 = Bundle.main.url(forResource: videoName2, withExtension: "mov") {
+            let player2 = AVPlayer(url: videoURL2)
+            let playerViewController2 = AVPlayerViewController()
+            playerViewController2.player = player2
+            playerViewController2.view.frame = view.bounds
+            playerViewController2.showsPlaybackControls = true
+            
+            addChild(playerViewController2)
+            view.addSubview(playerViewController2.view)
+            playerViewController2.didMove(toParent: self)
+            
+            player2.play()
+        }
+    }
+}
