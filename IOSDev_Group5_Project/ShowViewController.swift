e6d455691b8a82d0de1586cc01cedd689e90242e
Amirsha Kabulov <kabulovamirsha@gmail.com>
2023-05-04 11:28:38 -0500

    fixed video file and updated code

--- IOSDev_Group5_Project/ShowViewController.swift
+++ IOSDev_Group5_Project/ShowViewController.swift
@@ -15,7 +15,7 @@ class ShowViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let videoName = videoName, let videoURL = Bundle.main.url(forResource: videoName, withExtension: "mov") {
+        if let videoName = videoName, let videoURL = Bundle.main.url(forResource: videoName, withExtension: "mp4") {
             let player = AVPlayer(url: videoURL)
             let playerViewController = AVPlayerViewController()
             playerViewController.player = player
@@ -29,7 +29,7 @@ class ShowViewController: UIViewController {
             player.play()
         }
         
-        if let videoName2 = videoName2, let videoURL2 = Bundle.main.url(forResource: videoName2, withExtension: "mov") {
+        if let videoName2 = videoName2, let videoURL2 = Bundle.main.url(forResource: videoName2, withExtension: "mp4") {
             let player2 = AVPlayer(url: videoURL2)
             let playerViewController2 = AVPlayerViewController()
             playerViewController2.player = player2
